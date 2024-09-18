5c233c8a2514ac60c231dbdb89f3d22308e5aadf
talkin-ai <126031354+talkin-ai@users.noreply.github.com>
2024-09-17 20:10:21 -0700

    Update returnJobDescription.js

--- src/pages/api/returnJobDescription.js
+++ src/pages/api/returnJobDescription.js
@@ -20,16 +20,10 @@ const generateDescription = async ({
           },
           {
             role: "user",
-            content: `Please create a fantasy story in ${numPanels} centered on ${idea}. 
+            content: "Please create a fantasy story in ${numPanels} centered on ${idea}. 
             The character descriptions are ${characters}. For each panel, provide a single sentence describing both the setting and the action in fewer than 20 words. 
             Ensure each scene clearly integrates the setting and action into a straightforward sentence using simple language. 
-            Each scene should have a distinct and clear setting. 
-            Example of comic description: Panel 1: Raya and Finn stand at the edge of a dense jungle, sunlight filtering through the trees as Raya holds an old treasure map. 
-            Panel 2: Finn clears thick vines with a machete while Raya, with keen gray eyes, carefully follows the map's directions. 
-            Panel 3: They uncover a moss-covered stone pedestal hidden by vines, with Raya brushing away the foliage to reveal ancient symbols. 
-            Panel 4: Raya presses the symbols on the pedestal, causing a hidden compartment to click open and reveal a dusty key. 
-            Panel 5: Finn shines a flashlight into the jungle’s dense undergrowth, revealing a narrow, hidden path leading to a concealed cave entrance. 
-            Panel 6: Inside the cave, Raya and Finn find an ornate chest on a stone pedestal, their faces glowing with excitement and anticipation.',
+            Each scene should have a distinct and clear setting. ",
           },
         ],
         max_tokens: 300,
@@ -56,6 +50,4 @@ export default async function handler(req, res) {
   res.status(200).json({
     storyDescription,
   });
-  
-  
 }
