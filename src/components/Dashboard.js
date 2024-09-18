40749cc876fcf51a399d8e1398e91c91ba13c52f
talkin-ai <126031354+talkin-ai@users.noreply.github.com>
2024-09-17 21:12:39 -0700

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -40,9 +40,16 @@ export default function Dashboard() {
         <div className="">
           <form onSubmit={(e) => handleSubmit(e)}>
             <div className="flex flex-col">
-              <label className="sr-only" htmlFor="jobTitle">
-                Characters
-              </label>
+              <div className="flex mt-10 items-center space-x-3">
+                         <div
+            style={{ backgroundColor: '#1A6292'}}
+            className="rounded-full w-8 h-8 flex items-center justify-center text-white font-bold text-sm">
+            {1}
+        </div>
+                        <p className="text-left font-medium flex align-center">
+                            {"Characters"} 
+                        </p>
+                    </div>
               <input
                 type="text"
                 className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
